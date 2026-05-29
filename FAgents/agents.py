5c834086fce3a3fbca7664e7517f262add6256f1
sysfab <sysfab64@gmail.com>
2026-05-29 16:55:03 +0300

    chore: reformat

--- FAgents/agents.py
+++ FAgents/agents.py
@@ -46,7 +46,11 @@ class Tool[**P, R]:
         self.Name: str = func.__name__
         self.Description: str = inspect.cleandoc(func.__doc__ or "")
         self.Signature: inspect.Signature = inspect.signature(func)  # type: ignore[arg-type]
-        self.IsAwaitable = inspect.iscoroutinefunction(func) or inspect.isasyncgenfunction(func) or inspect.isawaitable(func)
+        self.IsAwaitable = (
+            inspect.iscoroutinefunction(func)
+            or inspect.isasyncgenfunction(func)
+            or inspect.isawaitable(func)
+        )
         wraps(func)(self)  # type: ignore[arg-type]
 
     @property
